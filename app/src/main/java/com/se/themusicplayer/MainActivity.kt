b13ef03b6a1e5ed11dc2834e1c0c26b1e89493b0
Endless <64025548+raccoon666666@users.noreply.github.com>
2022-04-24 23:43:10 +0800

    开场动画+按键动画

--- app/src/main/java/com/se/themusicplayer/MainActivity.kt
+++ app/src/main/java/com/se/themusicplayer/MainActivity.kt
@@ -1,16 +1,44 @@
 package com.se.themusicplayer
 
+import android.animation.Animator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.se.themusicplayer.databinding.ActivityMainBinding
+
 import com.se.themusicplayer.ui.player.PlayerActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Intent(this,PlayerActivity::class.java).let {
-            startActivity(it)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.startAppAnimation.apply{
+            repeatCount = 0
+            playAnimation()
+            speed = 1f
+            addAnimatorListener(object :Animator.AnimatorListener{
+                override fun onAnimationStart(animation: Animator?) {
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    Intent(this@MainActivity,PlayerActivity::class.java).let{
+                        startActivity(it)
+                    }
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                }
+                override fun onAnimationRepeat(animation: Animator?) {
+                }
+            })
         }
+
+
     }
+
 }
--- app/src/main/java/com/se/themusicplayer/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/se/themusicplayer/ui/player/PlayerActivity.kt
@@ -1,22 +1,83 @@
 package com.se.themusicplayer.ui.player
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.se.themusicplayer.R
 import com.se.themusicplayer.databinding.ActivityPlayerBinding
 import com.se.themusicplayer.ui.dialog.PlaylistDialog
 
 class PlayerActivity : AppCompatActivity() {
     private lateinit var binding:ActivityPlayerBinding
+    companion object{
+        val REPEAT_PLAY = 0
+        val RANDOM_PLAY = 1
+        val SEQUENCE_PLAY =2
+    }
+    //暂停否
+    private var isPause = false
+    //循环模式
+    private var repeatMode = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
 
+
+
         binding.listOfSongs.apply {
             setOnClickListener {
                 PlaylistDialog().show(supportFragmentManager,null)
             }
+        }
+        binding.playMode.apply{
+            setOnClickListener {
+                repeatMode = (repeatMode + 1) % 3
+                when (repeatMode) {
+                    0 -> R.drawable.ic_repeat_play
+                    1 -> R.drawable.ic_random_play
+                    else -> R.drawable.ic_sequence_play
+                }.let { setImageResource(it) }
+            }
+        }
+        binding.nextSong.apply {
+            speed = 2f
+            setOnClickListener { playAnimation() }
+        }
+        binding.previousSong.apply {
+            speed = 2f
+            setOnClickListener { playAnimation() }
+        }
+        binding.musicPlay.apply{
+            speed = 2f
+            setOnClickListener {
+                if (isPause) resumeAnimation()
+                else playAnimation()
+            }
+            addAnimatorUpdateListener {
+                println(isPause)
+                if(!isPause && it.animatedFraction > 0.5f) {
+                    isPause = !isPause
+                    pauseAnimation()
+                }
+            }
+            addAnimatorListener(object: Animator.AnimatorListener{
+                override fun onAnimationStart(animation: Animator?) {
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    isPause = false
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                }
+
+                override fun onAnimationRepeat(animation: Animator?) {
+                }
+
+            })
         }
     }
 
